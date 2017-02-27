05ac9d2dbd7a81212ecb95be1809345d4fbe99cd
keelimeguy <keelin.becker-wheeler@uconn.edu>
2017-02-27 00:34:56 -0500

    Minor improvements.

--- home_finding_robot.c
+++ home_finding_robot.c
@@ -6,29 +6,29 @@
 #include "home_finding_robot.h"
 
 void moveForward(void) {
-     // Right motor
-    DDRB = (1<<DDB2)|(1<<DDB1)|(1<<DDB0); // Sets B0, B1, B2 as output pins
-    PORTB = (1<<PORTB2)|(1<<PORTB0); // Turn right motors on, forward
+    // Right motor
+    DDRB |= (1<<DDB2)|(1<<DDB1)|(1<<DDB0); // Sets B0, B1, B2 as output pins
+    PORTB |= (1<<PORTB2)|(1<<PORTB0); // Turn right motors on, forward
     PORTB &= ~(1<<PORTB1); // Turn B1 off
     // Left motor
-    DDRD = (1<<DDD5)|(1<<DDD6)|(1<<DDD7); // Sets D5, D6, D7 as output pins
-    PORTD = (1<<PORTD5)|(1<<PORTD6); // Turn left motors on, forward
+    DDRD |= (1<<DDD5)|(1<<DDD6)|(1<<DDD7); // Sets D5, D6, D7 as output pins
+    PORTD |= (1<<PORTD5)|(1<<PORTD6); // Turn left motors on, forward
     PORTD &= ~(1<<PORTD7); // Turn D7 off
 }
 
 void moveBackwards(void) {
-     // Right motor
-    DDRB = (1<<DDB2)|(1<<DDB1)|(1<<DDB0); // Sets B0, B1, B2 as output pins
-    PORTB = (1<<PORTB2)|(1<<PORTB1); // Turn right motors on, reverse
+    // Right motor
+    DDRB |= (1<<DDB2)|(1<<DDB1)|(1<<DDB0); // Sets B0, B1, B2 as output pins
+    PORTB |= (1<<PORTB2)|(1<<PORTB1); // Turn right motors on, reverse
     PORTB &= ~(1<<PORTB0); // Turn B0 off
     // Left motor
-    DDRD = (1<<DDD5)|(1<<DDD6)|(1<<DDD7); // Sets D5, D6, D7 as output pins
-    PORTD = (1<<PORTD5)|(1<<PORTD7); // Turn left motors on, reverse
+    DDRD |= (1<<DDD5)|(1<<DDD6)|(1<<DDD7); // Sets D5, D6, D7 as output pins
+    PORTD |= (1<<PORTD5)|(1<<PORTD7); // Turn left motors on, reverse
     PORTD &= ~(1<<PORTD6); // Turn D6 off
 }
 
 void rotateRight(void) {
-     // Right motor
+    // Right motor
     DDRB |= (1<<DDB2)|(1<<DDB1)|(1<<DDB0); // Sets B0, B1, B2 as output pins
     PORTB |= (1<<PORTB2)|(1<<PORTB1); // Turn right motors on, reverse
     PORTB &= ~(1<<PORTB0); // Turn B0 off
@@ -39,7 +39,7 @@ void rotateRight(void) {
 }
 
 void rotateLeft(void) {
-     // Right motor
+    // Right motor
     DDRB |= (1<<DDB2)|(1<<DDB1)|(1<<DDB0); // Sets B0, B1, B2 as output pins
     PORTB |= (1<<PORTB2)|(1<<PORTB0); // Turn right motors on, forward
     PORTB &= ~(1<<PORTB1); // Turn B1 off
